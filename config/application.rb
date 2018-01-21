7239ad6219ad5744d669972035bca87b2b641039
Jinsol <solwish90@gmail.com>
2018-01-21 13:36:42 +0000

    modify application.rb for assets config

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module Gemtaku
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.assets.enabled = true
+    config.assets.precompile = %w(*.js *.coffee *.scss *.css *.png *.jpg *.jpeg *.gif *.ico *.eot *.svg *.ttf *.woff)
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Seoul'
