6766122b3e396eafd718d17c759f2f16416f1988
solwish <solwish90@gmail.com>
2018-01-24 14:49:28 +0000

    improve UI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @new_shop = Shop.where('created_at >= ?', 1.week.ago).count
+    @shop = Shop.all.count
     @escape = Escape.all.count
     # @club = Club.all.count
   end
